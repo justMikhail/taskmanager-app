663267b780502b1b0db31133939903e2be5ff6f1
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-09-01 02:53:48 +0300

    Use "AbstractView" for simple components(classes)

--- src/view/task-list.js
+++ src/view/task-list.js
@@ -1,26 +1,11 @@
-import {createElement} from '../utils/utils.js';
+import AbstractView from './abstract';
 
 const createTaskListTemplate = () => '<div class="board__tasks"></div>';
 
-export default class TaskList {
-  constructor() {
-    this._element = null;
-  }
+export default class TaskList extends AbstractView {
 
   getTemplate() {
     return createTaskListTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
 
