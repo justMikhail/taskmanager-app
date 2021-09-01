cecc073b5a52ddc8d83c505509f51289b32fd403
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-09-01 03:20:41 +0300

    Add engine for listeners in components

--- src/view/task.js
+++ src/view/task.js
@@ -1,5 +1,5 @@
 import AbstractView from './abstract';
-import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate, createElement} from '../utils/utils';
+import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from '../utils/utils';
 
 const createTaskTemplate = (task) => {
 
@@ -76,9 +76,20 @@ export default class Task extends AbstractView {
   constructor(task) {
     super();
     this._task = task;
+    this._editClickHandler = this._editClickHandler.bind(this);
   }
 
   getTemplate() {
     return createTaskTemplate(this._task);
+  }
+
+  _editClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.editClick();
+  }
+
+  setEditClickHandler(callback) {
+    this._callback.editClick = callback;
+    this.getElement().querySelector('.card__btn--edit').addEventListener('click', this._editClickHandler);
   }
 }
