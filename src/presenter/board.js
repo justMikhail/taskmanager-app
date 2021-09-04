655ef0ad8b7831608413ee7a137d3b32dc47ab61
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-09-04 17:16:27 +0300

    add open task limits (update "presenter")

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -24,6 +24,7 @@ export default class Board {
     this._loadMoreButtonComponent = new LoadMoreButtonView();
 
     this._handleTaskChange = this._handleTaskChange.bind(this);
+    this._handleModeChange = this._handleModeChange.bind(this);
     this._handleLoadMoreButtonClick = this._handleLoadMoreButtonClick.bind(this);
   }
 
@@ -33,6 +34,10 @@ export default class Board {
     render(this._boardComponent, this._taskListComponent, RenderPosition.BEFORE_END);
 
     this._renderBoard();
+  }
+
+  _handleModeChange() {
+    this._taskPresenter.forEach((presenter) => presenter.resetView());
   }
 
   _handleTaskChange(updatedTask) {
@@ -45,7 +50,7 @@ export default class Board {
   }
 
   _renderTask(task) {
-    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange);
+    const taskPresenter = new TaskPresenter(this._taskListComponent, this._handleTaskChange, this._handleModeChange);
     taskPresenter.init(task);
     this._taskPresenter.set(task.id, taskPresenter);
   }
